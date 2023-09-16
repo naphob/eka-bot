4593c6509f54e8c64ff67d96f781d421db6ea0ea
Naphob <naphob@gmail.com>
2023-09-16 14:03:32 +0700

    add font

--- cogs/welcomes.py
+++ cogs/welcomes.py
@@ -18,15 +18,15 @@ class Welcomes(commands.Cog):
 
     async def welcome_pic(self, user):
         channel = await self.bot.fetch_channel(WELCOME_CHANNEL_ID)
-        await user.display_avatar.save('Asset/avatar.png')
-        avatar = Image.open('Asset/avatar.png')
+        await user.display_avatar.save('assets/avatar.png')
+        avatar = Image.open('assets/avatar.png')
         count = user.guild.member_count
         username = user.name
-        text = f"Welcome {username} to IDS"
+        text = f"WELCOME {username} to eKA ESPORTS"
         member_text = f"Member #{count}"
-        img =Image.open("Asset/ids_bg.png")
+        img =Image.open("assets/welcome_bg.png")
         W, H = img.size
-        avatar = Image.open("Asset/avatar.png")
+        avatar = Image.open("assets/avatar.png")
         size = (240, 240)
         avatar = avatar.resize(size, Image.Resampling.LANCZOS)
         mask_img = Image.new("L", avatar.size, 0)
@@ -34,17 +34,17 @@ class Welcomes(commands.Cog):
         mask_draw.ellipse((0, 0) + avatar.size, fill=255)
         mask_img.save("assets/mask_circle.jpg", quality=95)
         draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("arial.ttf", 50)
-        count_font = ImageFont.truetype("arial.ttf", 32)
+        font = ImageFont.truetype("~/.fonts/Poppins-Bold.ttf", 50)
+        # count_font = ImageFont.truetype("arial.ttf", 32)
 
         img.paste(avatar ,(440, 80), mask_img)
         text_size =draw.textlength(text, font=font)
-        count_size =draw.textlength(member_text, font=count_font)
+        # count_size =draw.textlength(member_text, font=count_font)
         draw.text(((W-text_size)/ 2, 340), text, fill=(255, 255, 255, 255), font=font, aligh="center")
-        draw.text(((W-count_size)/ 2, 400), member_text, fill="grey", font=count_font, aligh="center")
+        # draw.text(((W-count_size)/ 2, 400), member_text, fill="grey", font=count_font, aligh="center")
         img.save('assets/text.png')
 
-        await channel.send(file= discord.File('Asset/text.png'))
+        await channel.send(file= discord.File('assets/text.png'))
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -55,16 +55,6 @@ class Welcomes(commands.Cog):
     @discord.slash_command(name="welcome", description="Welcome new member")
     async def welcome(self,ctx, user: discord.Member):
         await self.welcome_pic(user)
-
-    @discord.slash_command(name="role", description="Select a role")
-    async def role(self, ctx):
-        embed = discord.Embed(
-                title = "Welcome to the verse",
-                description=f"เลือก Role ที่เหมาะสมกับคุณ เพื่อใช้ในการสื่อสารภายในดิสคอร์ด",
-                color=discord.Color.dark_purple()
-            )
-
-        await ctx.respond(embed=embed, view=GetRoles())
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(Welcomes(bot)) # add the cog to the bot
